9cdfb255491307cc75d0de367155ecaad9d75e7c
Martin Marinov <martin.marinov10@yahoo.com>
2025-04-17 23:45:28 +0100

    Login template and routing setup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import page from 'page';
+
 import homeView from './views/home';
 import petsView from './views/pets';
+import loginView from './views/login';
 import layoutView from './views/layout';
 
 // Middlewares
@@ -9,6 +11,7 @@ page(layoutView);
 // Setup routes
 page('/', homeView);
 page('/pets', petsView);
+page('/login', loginView);
 
 // Start routing
 page();
