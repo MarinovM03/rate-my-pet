37477724e5361f2700d79d10ef5d062d46f9eab0
Martin Marinov <martin.marinov10@yahoo.com>
2025-04-17 23:33:52 +0100

    Create pets view and register it, extract home content in home view.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import page from 'page';
-import homeView from './views/home.js';
+import homeView from './views/home';
+import petsView from './views/pets';
+import layoutView from './views/layout';
+
+// Middlewares
+page(layoutView);
 
 // Setup routes
 page('/', homeView);
+page('/pets', petsView);
 
 // Start routing
 page();
---
+++ src/views/pets.js
@@ -0,0 +1,10 @@
+import { html } from 'lit-html';
+
+const template = () => html`
+    <h3>This is the Pets view!</h3>
+`;
+
+
+export default function (ctx) {
+    ctx.render(template());
+}
