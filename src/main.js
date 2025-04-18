b16d5fbeb6c9cd9a8f192a11420673cdbc8b924c
Martin Marinov <martin.marinov10@yahoo.com>
2025-04-18 01:05:38 +0100

    Add auth middleware

--- src/main.js
+++ src/main.js
@@ -5,8 +5,10 @@ import homeView from './views/home';
 import petsView from './views/pets';
 import loginView from './views/login';
 import layoutView from './views/layout';
+import { authMiddleware } from './middlewares/auth';
 
 // Middlewares
+page(authMiddleware);
 page(layoutView);
 
 // Setup routes
